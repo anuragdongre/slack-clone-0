6efa35bcbb10dcd2a4f01591ba790f5aa42f2f04
anuragdongre <anuragdongre555@gmail.com>
2023-07-07 14:09:57 +0530

    h

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { Avatar } from '@mui/material';
-// import { AccessTime ,HelpOutline,SearchOutlined} from "@material-ui/icons";
+import { Avatar } from "@mui/material";
+
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import SearchIcon from '@mui/icons-material/Search';
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import SearchIcon from "@mui/icons-material/Search";
 const Header = () => {
   const [user] = useAuthState(auth);
 
@@ -14,22 +14,23 @@ const Header = () => {
     <HeaderContainer>
       {/* Header Left */}
       <HeaderLeft>
-        <HeaderAvatar onClick={() => auth.signOut()}
-        src={user?.photoURL}
-        alt={user?.displayName}
+        <HeaderAvatar
+          onClick={() => auth.signOut()}
+          src={user?.photoURL}
+          alt={user?.displayName}
         />
-        <AccessTimeIcon/>
+        <AccessTimeIcon />
       </HeaderLeft>
 
       {/* Header Search */}
-        <HeaderSearch>
-          <SearchIcon/>
+      <HeaderSearch>
+        <SearchIcon />
 
-          <input placeholder="Search God Zues"/>
-        </HeaderSearch>
+        <input placeholder="Search God Zues" />
+      </HeaderSearch>
       {/* Header Right */}
       <HeaderRight>
-        <HelpOutlineIcon/>
+        <HelpOutlineIcon />
       </HeaderRight>
     </HeaderContainer>
   );
@@ -38,63 +39,62 @@ const Header = () => {
 export default Header;
 
 const HeaderSearch = styled.div`
-flex: 0%.4;
-opacity: 1;
-border-radius: 6px;
-background-color: #421f44;
-text-align: center;
-display: flex;
-padding: 0 50px;
-
-> input {
-  background-color: transparent;
-  border: none;
+  flex: 0%.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
   text-align: center;
-  min-width: 30vw;
-  outline: 0;
-  color: white;
-}
+  display: flex;
+  padding: 0 50px;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
 `;
 
 const HeaderContainer = styled.div`
-display: flex;
-position:fixed;
-width: 100%;
-align-items: center;
-justify-content: space-between;
-padding: 10px, 0;
-background-color: var(--slack-color);
-color: white;
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px, 0;
+  background-color: var(--slack-color);
+  color: white;
 `;
 
 const HeaderLeft = styled.div`
-flex: 0.3;
-display: flex;
-align-items:center ;
-margin-left: 20px;
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
 
-> .MuiSvgIcon-root {
-  margin-left: auto;
-  margin-right: 30px;
-}
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
 `;
 
 const HeaderRight = styled.div`
-flex: 0%.3;
-display: flex;
-align-items: flex-end;
+  flex: 0%.3;
+  display: flex;
+  align-items: flex-end;
 
-> .MuiSvgIcon-root {
-  margin-left: auto;
-  margin-right: 20px;
-
-}
-`
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
 
 const HeaderAvatar = styled(Avatar)`
-cursor:pointer;
+  cursor: pointer;
 
-:hover{
-  opacity: 0.8;
-}
+  :hover {
+    opacity: 0.8;
+  }
 `;
